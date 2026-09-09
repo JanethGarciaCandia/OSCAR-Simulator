68c70ec07f6f6620daae954875e851e0a87a9be3
Janeth Garcia Candia <ga477491@uaeh.edu.mx>
2026-09-09 13:31:08 -0600

    Actualización

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788972916|2755955529';
+const CACHE_VERSION = '1788981976|11816061550';
 /** @type {string} */
 const CACHE_PREFIX = 'Ahorasieslabuena-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
