476e7e84cd3d4e93f5cc0bc97327e2e7cdadee4c
Janeth Garcia Candia <ga477491@uaeh.edu.mx>
2026-09-09 16:01:27 -0600

    Actualización 3.0

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788989606|960971036';
+const CACHE_VERSION = '1788991244|65041805';
 /** @type {string} */
 const CACHE_PREFIX = 'OSCAR Simulator-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
