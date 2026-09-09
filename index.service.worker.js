0f1b5f812f34661cd7da55814b16e1ad94a09d34
Janeth Garcia Candia <ga477491@uaeh.edu.mx>
2026-09-09 11:06:15 -0600

    Actualización Para celulares

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788927856|83925078';
+const CACHE_VERSION = '1788972916|2755955529';
 /** @type {string} */
 const CACHE_PREFIX = 'Ahorasieslabuena-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
